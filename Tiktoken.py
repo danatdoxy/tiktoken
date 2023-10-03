f07f740442956dc44a319ae331d2850c0a81980b
danatdoxy <dan@doxy.me>
2023-10-03 11:52:39 -0400

    Updated procfile and debugged tiktoken logic

--- Tiktoken.py
+++ Tiktoken.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, abort
-from tiktoken import Tokenizer
+import tiktoken
 
 app = Flask(__name__)
-tokenizer = Tokenizer()
+enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
 SECRET = "Xujf9jgg1z"
 
@@ -11,11 +11,9 @@ def count_tokens():
     auth_header = request.headers.get('Authorization')
     if not auth_header or auth_header != SECRET:
         abort(401)  # Unauthorized
-
     data = request.json
     text = data.get('text', '')
-    
-    token_count = tokenizer.count_tokens(text)
+    token_count = len(enc.encode(text))
     return jsonify({"token_count": token_count})
 
 if __name__ == '__main__':
